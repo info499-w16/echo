65fc3c8040b0b429e80bc1b9cc602218b77ebf1c
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-01-21 18:30:27 -0800

    Implemented UDP registration
    This is what will allow for discovery in our service. In addition to
    it's regular functionalities, the service broadcasts metadata
    information about itself over the entire network. It doesn't care if
    other services listen to that data or do anything with it, so it's
    still really self contained.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,44 @@
 'use strict'
 
+const dgram = require('dgram')
 const http = require('http')
+const uuid = require('uuid')
+
+const REGISTRATION_PORT = process.env.REGISTRATION_PORT || 8888
+
+function timeLog (msg) {
+  console.log(`${new Date().toUTCString()}: ${msg}`)
+}
 
 http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'application/json'})
   response.end('{"hello": "world"}\n')
-  console.log(request)
+  timeLog.log('Served Request')
 }).listen(80)
 
-console.log('Echo listening on http://localhost:80')
+timeLog('Echo listening on http://localhost')
+
+const client = dgram.createSocket('udp4')
+
+client.bind(REGISTRATION_PORT, () => {
+  // This is critical for properly functioning, since the whole point is that
+  // we DONT know what might be listening
+  client.setBroadcast(true)
+
+  const instanceData = JSON.stringify({
+    name: 'echo',
+    version: '0.0.1',
+    id: uuid.v4()
+  })
+
+  function sendRegistrationData () {
+    client.send(instanceData, 0, instanceData.length, REGISTRATION_PORT, '255.255.255.255', (err) => {
+      if (err) throw err
+
+      timeLog('Sent registration data')
+    })
+  }
+
+  sendRegistrationData()
+  setInterval(sendRegistrationData, 1000 * 30) // Send every 30 seconds
+})
